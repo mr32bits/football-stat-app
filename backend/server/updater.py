74b3087bb2f85fe616a5105695153ed81507239a
mr32bits <169852035+mr32bits@users.noreply.github.com>
2025-11-06 00:22:31 +0100

    Update updater.py

--- backend/server/updater.py
+++ backend/server/updater.py
@@ -174,7 +174,7 @@ def custom_windows_install(
         robocopy "%SRC%" "%DST%" /E /IS /IT /MOVE /R:5 /W:2 >nul
 
         echo Restarting app...
-        start "FootballStats" /d "%DST%" FootballStats.exe
+        start "" "%DST%\\FootballStats.exe"
 
         echo Cleaning up temp files...
         rmdir /S /Q "%SRC%" >nul
